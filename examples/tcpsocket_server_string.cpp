79b3073a128f801da500f0b23a9dd500c20cc8d5
miaodx <miaodx@hotmail.com>
2017-06-20 15:48:15 +0800

    use unique_ptr to avoid potential memory leak.

--- examples/tcpsocket_server_string.cpp
+++ examples/tcpsocket_server_string.cpp
@@ -4,23 +4,22 @@
 
 using namespace std;
 
-int main(){
-    //fr::HttpSocket<fr::TcpSocket> client; //fr::TcpSocket for HTTP. fr::SSLSocket for HTTPS.
+int main() {
 
-	fr::TcpSocket client;
 	
+	fr::TcpSocket client;
+	fr::TcpListener listener;             //Use an fr::SSLListener if HTTPS.
 
-    fr::TcpListener listener;             //Use an fr::SSLListener if HTTPS.
 
 	string port = "8081";
 
-    //Bind to a port
-    if(listener.listen(port) != fr::Socket::Success)
-    {
+	//Bind to a port
+	if (listener.listen(port) != fr::Socket::Success)
+	{
 		cout << "LISTENER:Failed to bind to port, going to shutdown" << endl;
 		listener.shutdown();
 		return -1;
-    }
+	}
 
 	cout << "LISTENER:Listener is listening on port " << port << " ..." << endl;
 
@@ -41,23 +40,26 @@ int main(){
 		while (true) // infinite loop for the communication
 		{
 			try
-			{							
+			{
 				size_t received_size;
 
-				//char recv_str_buffer[RECV_CHUNK_SIZE];
-				char* recv_str_buffer = new char[RECV_CHUNK_SIZE];
-				
-				status = client.receive_raw(recv_str_buffer, RECV_CHUNK_SIZE, received_size);
+				/*
+					To avoid potential memory leak, use unique_ptr here [as suggested by Cloaked9000](https://github.com/MiaoDX-fork-and-pruning/frnetlib/commit/080826bc61791f8b6f7881501795ac7acdc668dd#commitcomment-22636346)
+				*/
 
-				if (status != fr::Socket::Success ) {
+				std::unique_ptr<char[]> recv_str_buffer(new char[RECV_CHUNK_SIZE]);
+				status = client.receive_raw(recv_str_buffer.get(), RECV_CHUNK_SIZE, received_size);
+
+				if (status != fr::Socket::Success) {
 					cout << "LISTENER:Failed to receive request, we got status: " << status << endl;
 					break;
 				}
 
-				string recv_str(recv_str_buffer);
-				recv_str.resize(received_size);				
+				string recv_str(recv_str_buffer.get());
+				recv_str.resize(received_size);
 
-				cout << "LISTENER:We got from client:" << recv_str << ", size is: "<< received_size << endl;
+
+				cout << "LISTENER:We got from client:" << recv_str << ", size is: " << received_size << endl;
 
 				status = client.send_raw(recv_str.c_str(), recv_str.length());
 				if (status != fr::Socket::Success) {
